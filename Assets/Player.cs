5775d881397c2c6672da15e6f5a007e7c9a191b1
vanessa4512 <vanesita1208@gmail.com>
2026-05-13 20:19:44 -0600

    personaje mira a ambos lados

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,17 +2,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float movement;
+    private float movement;
+    public  float moveSpeed = 5f;
+    private bool  facingRight;
+
+    public float jumpHeight = 7f;
+    public Rigidbody2D rb;
+    private bool isGround;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update() {
         movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement * Time.deltaTime,0f,0f);
+        transform.position += new Vector3(movement * Time.deltaTime * moveSpeed,0f,0f);
+
+        if (movement < 0f && facingRight == true)
+        {
+            transform.eulerAngles = new Vector3(0f, -180, 0f);
+            facingRight = false;
+        }
+
+        else if (movement > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingRight = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+            isGround = false;
+        }
+
+        void Jump() {
+            Vector2 velocity = rb.linearVelocity;
+            velocity.y = jumpHeight;
+            rb.linearVelocity = velocity;
+        }
     }
 }
