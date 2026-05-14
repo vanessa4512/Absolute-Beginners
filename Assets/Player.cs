9b07e50b2b62cc15e9957b48638b0caae166cc9e
vanessa4512 <vanesita1208@gmail.com>
2026-05-13 19:04:28 -0600

    movimiento leve y actualizacion de assets

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement * Time.deltaTime,0f,0f);
     }
 }
