ebecbcdc3879c4ab11e6049e56aa27471dc0a145
vanessa4512 <vanesita1208@gmail.com>
2026-05-20 13:55:54 -0600

    el enemigo se mueve ya solo en cierta area

--- Assets/scrips/Enemy.cs
+++ Assets/scrips/Enemy.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float walkSpeed = 2.5f;
     private bool facingLeft;
+
+    public Transform detectPoint;
+    public float distance = 0.5f;
+    public LayerMask detectLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +17,31 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(new Vector2(-1f, 0f)* Time.deltaTime * walkSpeed);
 
+        RaycastHit2D hitInfo = Physics2D.Raycast(detectPoint.position, new Vector2(0f, -1f), distance, detectLayer);
+
+        if (hitInfo == false)
+        {
+            if (facingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+                facingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                facingLeft = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (detectPoint == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(detectPoint.position, Vector3.down * distance);
     }
 }
